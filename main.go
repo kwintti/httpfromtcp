b990f2398f86eaba78cab5240e6677a43c9a4e13
kwintti <sergei.borovskii@gmail.com>
2025-05-21 18:59:05 +0300

    now reading data from tcp connection.

--- main.go
+++ main.go
@@ -1,34 +1,56 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"io"
 	"log"
-	"os"
+	"net"
 	"strings"
+	"time"
 )
 
 func main() {
-	f, err := os.Open("messages.txt")
+	l, err := net.Listen("tcp", ":42069")
 	if err != nil {
 		log.Fatal(err)
 	}
-	for line := range getLinesChannel(f) {
-		fmt.Printf("read: %s \n", line)
-	}
+	defer l.Close()
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println("Connection have been accepted")
+
+		for line := range getLinesChannel(conn) {
+			fmt.Printf("%s\n", line)
+		}
+		fmt.Println("Connection is closed")
+	 }
 }
 
-func getLinesChannel(f io.ReadCloser) <-chan string {
+func getLinesChannel(f net.Conn) <-chan string {
 	linesChan := make(chan string)
 	go func(){
-		defer close(linesChan)
 		defer f.Close()
+		defer close(linesChan)
 		currentLine := ""
 		slice := make([]byte, 8)
 		for {
+			err := f.SetDeadline(time.Now().Add(time.Second))
 			n, err := f.Read(slice)
+			if n == 0 {
+				toString := string(slice[:n])
+				currentLine += toString
+				linesChan <- currentLine
+				break
+			}
 			if err != nil {
-				if err == io.EOF {
+				if errors.Is(err, io.EOF) {
+					toString := string(slice[:n])
+					currentLine += toString
+					linesChan <- currentLine
 					break
 				}
 				log.Fatal(err)
@@ -42,10 +64,6 @@ func getLinesChannel(f io.ReadCloser) <-chan string {
 				continue
 			}
 		}
-		if currentLine != "" {
-			linesChan <- currentLine
-		}
-
 	}()
 	return linesChan
 }
