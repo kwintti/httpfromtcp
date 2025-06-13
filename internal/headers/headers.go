2a7d361256e7ea3bc22a183d83baa3e30fd88e5c
kwintti <sergei.borovskii@gmail.com>
2025-06-14 00:04:15 +0300

    added ability to add multiple field-values

--- internal/headers/headers.go
+++ internal/headers/headers.go
@@ -3,6 +3,8 @@ package headers
 import (
 	"bytes"
 	"fmt"
+	"regexp"
+	"strings"
 )
 
 type Headers map[string]string
@@ -22,12 +24,33 @@ func (h Headers) Parse(data []byte) (n int, done bool, err error) {
 	}
 	BytesRead := len(before) + 2
 	fieldName, fieldValue, _ := bytes.Cut(before, []byte(":"))
+	if len(fieldName) == 0 {
+		return 0, false, fmt.Errorf("field-name is missing")
+	}
 	if fieldName[len(fieldName)-1] == ' ' {
 		return 0, false, fmt.Errorf("Spaces between field-name and colon: %v", err)
 	}
-	trimmedFieldValue := bytes.Trim(fieldValue, " ")
-	trimmedFieldName := bytes.TrimLeft(fieldName, " ")
-	h[string(trimmedFieldName)] = string(trimmedFieldValue)
+	trimmedFieldValue := string(bytes.Trim(fieldValue, " "))
+	trimmedFieldName := string(bytes.TrimLeft(fieldName, " "))
+
+	
+	trimmedFieldName = strings.ToLower(trimmedFieldName)
+
+	re := regexp.MustCompile("[a-zA-Z0-9!#$%&'*+-.^_`|~]")
+	for _, c := range trimmedFieldName {
+		match := re.MatchString(string(c))
+		if !match {
+			return 0, false, fmt.Errorf("field-name contains invalid character")
+		}
+	}
+
+	val, ok := h[trimmedFieldName]
+	if ok {
+		h[trimmedFieldName] = val + ", " + trimmedFieldValue
+			return BytesRead, false, nil
+	}
+	
+	h[trimmedFieldName] = trimmedFieldValue
 	
 	return BytesRead, false, nil
 }
